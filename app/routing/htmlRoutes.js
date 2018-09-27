c900765e524e7db181cb1e3d87d7fa62c2ea92e3
Hifive6 <39879994+Hifive6@users.noreply.github.com>
2018-09-27 18:49:30 -0400

    deleled unnessary lines in htmlRoutes and add require to correct routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,9 +4,9 @@ var path = require("path");
 // var app = express()
 module.exports = function(app){
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(bodyParser.urlencoded({ extended: true}));
+// app.use(bodyParser.json());
 
 
 

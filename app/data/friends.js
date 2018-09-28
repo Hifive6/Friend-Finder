0abcca8cf6382de0331712cd606e49a103894d08
Hifive6 <39879994+Hifive6@users.noreply.github.com>
2018-09-27 20:55:24 -0400

    added data to friends file

---
+++ app/data/friends.js
@@ -0,0 +1,69 @@
+
+
+const friendsArray = [
+    {
+        name: "George Singer",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/1905_cartoon_of_Charlie_Murphy_as_a_prisoner.jpg/758px-1905_cartoon_of_Charlie_Murphy_as_a_prisoner.jpg",
+        scores: [
+            5,
+            1,
+            3,
+            5,
+            3,
+            2,
+            3,
+            1,
+            1,
+            1
+        ]
+    },
+    {
+        name: "James Fromwell",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Baby_Huey_en_una_escena_de_%27Pest_Pupil%27.png/240px-Baby_Huey_en_una_escena_de_%27Pest_Pupil%27.png",
+        scores: [
+            5,
+            1,
+            2,
+            3,
+            5,
+            2,
+            1,
+            1,
+            3,
+            5
+        ]
+    },
+    {
+        name: "Billy J. Joel",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Kaarlo_Kari_selfportrait.jpg/173px-Kaarlo_Kari_selfportrait.jpg",
+        scores: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Joseph Vans",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Pope_cartoon.jpg/179px-Pope_cartoon.jpg",
+        scores: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    }
+]
+module.exports = friendsArray;

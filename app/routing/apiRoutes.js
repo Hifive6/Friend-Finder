c5227343f687d2983c31fb8cee2b008971e2f8b7
Hifive6 <39879994+Hifive6@users.noreply.github.com>
2018-09-29 14:21:03 -0400

    comparing scores and picking correct match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,9 @@
+
 const path = require("path");
 const friendsData = require("../data/friends")
 module.exports = function(app){
     const userArr = [];
-    let matchFriend = 0;
+    //let matchFriend = 0;
 
     // console.log(userArr)
     app.get("/api/friends", function(req, res) {
@@ -19,30 +20,45 @@ app.post("/api/friends", function(req, res) {
     // console.log(newFriend)
     
     //const arrayNums = newFriend.scores
-    //  console.log(arrayNums)
+    //  console.log(friendsData)
+    var bestMatch = false;
      friendsData.forEach(item =>{
-        let scoresDiff = 0;
+         current_scores = item.scores;
+        //  console.log(current_scores);
+        
     // let friendsDiff = (Math.abs(parseInt(item.scores)))
         // console.log(friendsDiff)
-    
-
-   newScores.forEach(num=>{
-        scoresDiff += (Math.abs(parseInt(item.scores)-parseInt(num)))
-    //    console.log(scoresDiff)
-       
-   })
-   userArr.push(scoresDiff)
-    //console.log(userArr)
-})
-    userArr.forEach(element => {
-        if(element.userArr <= userArr[matchFriend]){
-            //matchFriend = 
-            console.log(matchFriend)
+        var friendTally = {};
+        friendTally.score = 0;
+        for (var i =0; i < current_scores.length; i++) {
+            difference = (Math.abs(parseInt(current_scores[i])-parseInt(newScores[i])));
+            friendTally.score += difference;
         }
+
+        console.log(friendTally);
+        if (!bestMatch || bestMatch.score > friendTally.score) {
+            bestMatch = item
+            bestMatch.score = friendTally.score
+        }
+
+        // newScores.forEach(num=>{
+        //      scoresDiff += (Math.abs(parseInt(item.scores)-parseInt(num)))
+        //     //    console.log(scoresDiff)
+            
+        // })
+        // userArr.push(scoresDiff)
+            //console.log(userArr)
     })
-    friendsData.push(req.body)
-    const newMatch = friendsData[matchFriend];
-    res.json(newMatch)
+    console.log(bestMatch)
+    // userArr.forEach(element => {
+    //     if(element.userArr <= userArr[matchFriend]){
+    //         //matchFriend = 
+    //         console.log(matchFriend)
+    //     }
+    // })
+    // friendsData.push(req.body)
+    // const newMatch = friendsData[matchFriend];
+    res.json(bestMatch)
    
     
     

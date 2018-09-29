aea49de670e2ee4de760ba239b6a3872ed41bc1a
Hifive6 <39879994+Hifive6@users.noreply.github.com>
2018-09-28 20:44:40 -0400

    trying to loop through my data and user data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,19 +9,25 @@ module.exports = function(app){
 
     
 app.post("/api/friends", function(req, res) {
-    const newFriend = req.body
-    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
-    const arrayNums = newFriend.scores
-     console.log(arrayNums)
-    arrayNums.forEach(num=>{
+    // const newFriend = req.body
+    const newScores = req.body.scores
+    // newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+    console.log(newScores)
+    // console.log(newFriend)
+    
+    //const arrayNums = newFriend.scores
+    //  console.log(arrayNums)
+    newScores.forEach(num=>{
+        // const scoresDiff = 0
         console.log(num)
-    })
+    
 
    friendsData.forEach(item => {
+    //    scoresDiff += 
        console.log(item)
        
    })
-
+})
     const formNumbers = (numbersArr) => {
         // arrayNums.forEach(item => {
         // let completeScore = item.
